6dbbb017c9b1b3ce78cf3c16081d8c3877e5a520
AzuralStrike <azural_heero_strike@hotmail.com>
2016-11-18 17:24:12 -0500

    Enemy dummy & spawner
    made dummy enemy class for you to enjoy, had an issue making the spawner
    class recognize it though so it is there but commented out, please feel
    free to confirm if it is acceptable or if I am missing anything.

---
+++ Source/Wanted_B01/EnemySpawner.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Wanted_B01.h"
+#include "EnemySpawner.h"
+#include "Enemy.h"
+
+
+// Sets default values
+AEnemySpawner::AEnemySpawner()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AEnemySpawner::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AEnemySpawner::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+	//spawnRateInSeconds++;
+
+}
+
+void AEnemySpawner::spawnEnemy()
+{
+	//if (spawnRateInSeconds == ?? && Enemies > ??)
+	//{
+	//		spawn enemy.
+	//}
+}
