aade476db9bcc3cfd504b93d27d2375812ab45da
Sydney Kim-Buu Lu <sydney.lu@mytfs.ca>
2016-12-02 21:52:22 -0500

    CollectibleObjectV1
    Collectible Blueprint and C++ class Version1

---
+++ Source/Wanted_B01/CollectibleObject.cpp
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Wanted_B01.h"
+#include "CollectibleObject.h"
+
+
+// Sets default values
+ACollectibleObject::ACollectibleObject()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	ColliderComponent = CreateDefaultSubobject<USphereComponent>(TEXT("ColliderComponent"));
+
+	RootComponent = ColliderComponent;
+
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh Component"));
+
+	MeshComponent->AttachTo(ColliderComponent);
+
+	MovementComponent = CreateDefaultSubobject<UMovementComponent>(TEXT("Movement Component"));
+
+	//Set up a notification for when this component overlaps something
+	ColliderComponent->OnComponentBeginOverlap.AddDynamic(this, &ACollectibleObject::OnOverlapBegin);
+
+}
+
+// Called when the game starts or when spawned
+void ACollectibleObject::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACollectibleObject::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
+void ACollectibleObject::OnOverlapBegin(UPrimitiveComponent * OverlappedComp, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
+{
+	if ((OtherActor != nullptr) && (OtherActor != this))
+	{
+		//GEngine->AddOnScreenDebugMessage(-1, 0.5f, FColor::White, FString::Printf(TEXT("Hello World!")));
+
+		Destroy();
+		SetLifeSpan(5);
+	}
+}
+
